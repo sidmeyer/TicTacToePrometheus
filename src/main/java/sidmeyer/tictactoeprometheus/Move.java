c86237ae2f252765599115e4a73de568e0f07dc7
Stanislav <stanislav.moroz@gmail.com>
2017-03-21 12:55:18 +0200

    Create repo and add classes.

---
+++ src/main/java/sidmeyer/tictactoeprometheus/Move.java
@@ -0,0 +1,10 @@
+package sidmeyer.tictactoeprometheus;
+
+/**
+ * Created by stas on 21.03.17.
+ */
+public class Move {
+    public Move turn(Board board) {
+        
+    }
+}
---
+++ src/main/java/sidmeyer/tictactoeprometheus/Player.java
@@ -0,0 +1,7 @@
+package sidmeyer.tictactoeprometheus;
+
+/**
+ * Created by stas on 21.03.17.
+ */
+public class Player {
+}
